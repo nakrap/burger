26c4babf20147d9ef926d6b4d01172aff3be8a50
Nick Akrap <nakrap@gmail.com>
2018-06-30 00:10:00 -0500

    added routes and js

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+### Seeds
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hangover Helper', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smothered', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Heavenly', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shroomalicious', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Mess', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Knockout', false);
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,48 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      // var newdevoured = $(this).data("newdevoured");
+  
+      var newdevouredState = {
+        devoured: true
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newdevouredState
+      }).then(function() {
+          console.log("changed devoured to", newdevouredState);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      // var name = $("[name=burger-name]").val().trim();
+
+      // if(name !== "") {
+        var newBurger = {
+          name: $("[name=burger-name]").val().trim()
+        };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        });
+      // } else {
+      //   $("[name=burger-name]").val().trim();
+      // };
+    });
+  });
+  
